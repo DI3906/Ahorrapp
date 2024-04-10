f223a0e6beff7918238ab5cecac6dfac1e92ac21
Diego Garcia <diegg066.99@gmail.com>
2024-04-10 17:29:30 -0600

    Agregando Fuentes, Colores

--- app/src/main/java/com/example/ahorrapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/ahorrapp/ui/theme/Type.kt
@@ -2,9 +2,21 @@ package com.example.ahorrapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.ahorrapp.R
+
+val inter = FontFamily(
+    Font(R.font.inter_regular, FontWeight.Normal)
+)
+
+val poppins = FontFamily(
+    Font(R.font.poppins_bold, FontWeight.Bold),
+    Font(R.font.poppins_medium, FontWeight.Medium)
+
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
